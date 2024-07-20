a65707863b4252351651aa9e4edab5be6f148b1a
connieyyy <con.yang4098@gmail.com>
2024-07-19 18:44:02 -0700

    created front page

--- JumpStartFroggy/App.js
+++ JumpStartFroggy/App.js
@@ -1,11 +1,46 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+} from "react-native";
+
+import lilypadlogin from "./assets/lilypadlogin.jpg";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Helloooo!!</Text>
+      <ImageBackground
+        source={lilypadlogin}
+        style={styles.backgroundImage}
+        resizeMode="cover"
+      >
+        <Text style={styles.title}>Welcome Back!</Text>
+        <Text style={styles.subtitle}>
+          Jump start into a healthy college life!
+        </Text>
+
+        <View style={styles.buttoncontainer}>
+          <TouchableHighlight
+            style={styles.button}
+            onPress={() => Alert.alert("Button Pressed!")}
+            underlayColor="#1e90ff"
+          >
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={styles.button}
+            onPress={() => Alert.alert("Button Pressed!")}
+            underlayColor="#1e90ff"
+          >
+            <Text style={styles.buttonText}>Register</Text>
+          </TouchableHighlight>
+        </View>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +49,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+  },
+  backgroundImage: {
+    flex: 1,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 40,
+    fontWeight: "bold",
+    alignItems: "center",
+  },
+  subtitle: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "500",
+    alignItems: "center",
+
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  button: {
+    backgroundColor: "#000000",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+
+  buttoncontainer: {
+    marginTop: 50,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "80%",
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontSize: 20,
   },
 });
